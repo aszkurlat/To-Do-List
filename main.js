0ea5d963fdc598e27153dafa73a5cd55b655da5d
aszkurlat <szkurlatagnieszka@gmail.com>
2022-03-09 14:11:53 +0100

    update todolist main.js file

--- main.js
+++ main.js
@@ -1,45 +1,103 @@
-const form = document.querySelector('form');
-const ul = document.querySelector('ul');
-let taskNumber = document.querySelector('span');
-const tasksList = document.getElementsByClassName('task');
-const input = document.querySelector('input');
-const searchInput = document.querySelector('input.search');
-let toDoList = [];
+const taskInput = document.querySelector(".task-input");
+const addTaskBtn = document.querySelector(".add-task-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
-const removeTask = (e) => {
-    const index = e.target.parentNode.dataset.key;
-    toDoList.splice(index, 1);
-    taskNumber.textContent = toDoList.length;
-    renderList();
-}
+const deleteTask = (e) => {
+  const task = e.target.parentElement;
+  if (e.target.classList.contains("trash-btn")) {
+    task.classList.add("fall-down");
+    task.addEventListener("transitionend", () => {
+      task.remove();
+    });
+  }
+};
+
+const editTask = (e) => {
+  const task = e.target;
+  const input = e.target.parentElement;
+  if (
+    task.innerHTML == `<i class="fas fa-edit"></i>` &&
+    !e.target.parentElement.classList.contains("completed")
+  ) {
+    task.innerHTML = `<i class="fas fa-solid fa-thumbs-up"></i>`;
+    input.firstChild.removeAttribute("readOnly");
+    input.firstChild.focus();
+    console.log(input);
+  } else {
+    task.innerHTML = `<i class="fas fa-edit"></i>`;
+    input.firstChild.setAttribute("readOnly", true);
+  }
+};
+
+const completeTask = (e) => {
+  if (e.target.classList.contains("complete-btn")) {
+    e.target.parentElement.classList.toggle("completed");
+  }
+};
 
 const addTask = (e) => {
-    e.preventDefault();
-    const taskTitle = input.value;
-    if (taskTitle === "") return;
-    let task = document.createElement('li');
-    task.className = "task";
-    task.innerHTML = taskTitle + " <button>Delete</button>"
-    toDoList.push(task);
-    renderList();
-    ul.appendChild(task);
-    input.value = "";
-    taskNumber.textContent = tasksList.length;
-    task.querySelector("button").addEventListener('click', removeTask);
-}
-const renderList = () => {
-    ul.textContent = "";
-    toDoList.forEach((task, key) => {
-        task.dataset.key = key;
-        ul.appendChild(task);
-    })
-}
-const searchTask = (e) => {
-    const searchText = e.target.value.toLowerCase();
-    let tasks = toDoList;
-    tasks = tasks.filter(li => li.textContent.toLowerCase().includes(searchText));
-    ul.textContent = "";
-    tasks.forEach(li => ul.appendChild(li));
-}
-searchInput.addEventListener('input', searchTask)
-form.addEventListener('submit', addTask);
+  e.preventDefault();
+  const text = taskInput.value;
+  if (text == "") alert("Please fill out the task");
+  else {
+    const div = document.createElement("div");
+    div.className = "todo-task";
+
+    const taskTitle = document.createElement("input");
+    taskTitle.setAttribute("readonly", true);
+    taskTitle.className = "todo-item";
+    taskTitle.value = text;
+    div.appendChild(taskTitle);
+
+    const completeBtn = document.createElement("button");
+    completeBtn.className = "complete-btn";
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+    div.appendChild(completeBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.className = "edit-btn";
+    editBtn.innerHTML = '<i class="fas fa-edit"></i>';
+    div.appendChild(editBtn);
+
+    const trashBtn = document.createElement("button");
+    trashBtn.className = "trash-btn";
+    trashBtn.innerHTML = `<i class="fas fa-trash"></i>`;
+    div.appendChild(trashBtn);
+
+    todoList.appendChild(div);
+
+    completeBtn.addEventListener("click", completeTask);
+    editBtn.addEventListener("click", editTask);
+    trashBtn.addEventListener("click", deleteTask);
+
+    taskInput.value = "";
+  }
+};
+const filterTodo = (e) => {
+  let toDoList = todoList.childNodes;
+  toDoList.forEach((task) => {
+    switch (e.target.value) {
+      case "all":
+        task.style.display = "flex";
+        break;
+      case "completed":
+        if (task.classList.contains("completed")) {
+          task.style.display = "flex";
+        } else {
+          task.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!task.classList.contains("completed")) {
+          task.style.display = "flex";
+        } else {
+          task.style.display = "none";
+        }
+        break;
+    }
+  });
+};
+
+addTaskBtn.addEventListener("click", addTask);
+filterOption.addEventListener("click", filterTodo);
